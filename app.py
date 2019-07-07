207326cd5ac8e25ce3568dc48a6809b7f0b50021
Casey Scott <csnz2018@hotmail.com>
2019-07-07 13:59:09 +1200

    make it better

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, redirect, request,url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME']='data'
+app.config['MONGO_URI']='mongodb+srv://casey:rOOtUser@cluster0-uoefk.mongodb.net/data?retryWrites=true&w=majority'
+
+mongo=PyMongo(app)
+
+@app.route('/')
+  
+@app.route('/get_recipes')
+def get_recipes():
+  return render_template('recipes.html', recipe= mongo.db.recipe.find())
+
+if __name__ == '__main__':
+  app.run(host=os.environ.get('IP'),
+          port=int(os.environ.get('PORT')),
+          debug=True)
+  
