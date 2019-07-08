05450d2ebaa46f96fbf73c0f86aa1da535048677
Casey Scott <csnz2018@hotmail.com>
2019-07-08 20:34:48 +1200

    allergen and cuisine files

--- app.py
+++ app.py
@@ -10,11 +10,61 @@ app.config['MONGO_URI']='mongodb+srv://casey:rOOtUser@cluster0-uoefk.mongodb.net
 
 mongo=PyMongo(app)
 
+# List of the cuisine categories
+cuisines_json = []
+with open("data/cuisine.json", "r") as file:
+    cuisines_json = json.load(file)
+
+
+# List of the allergen categories
+allergens_json = []
+with open("data/allergen.json", "r") as file:
+    allergens_json = json.load(file)
+
+
 @app.route('/')
   
 @app.route('/get_recipes')
 def get_recipes():
   return render_template('recipes.html', recipe= mongo.db.recipe.find())
+
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template(
+        "addrecipe.html",
+        cuisines_json=cuisines_json,
+        allergens_json=allergens_json)
+
+
+@app.route("/recipes")
+def recipes():
+  
+  return render_template(
+        'recipes.html',
+        all_recipes=all_recipes,
+        new_recipes=new_recipes,
+        most_popular_recipes=most_popular_recipes,
+        cuisines_json=cuisines_json,
+        allergens_json=allergens_json,
+        usernames=usernames)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
   app.run(port=5000,
