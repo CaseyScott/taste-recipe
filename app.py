ddb77d7c2e29a4f284d1fa074f005dacb276370a
Casey Scott <csnz2018@hotmail.com>
2019-07-09 17:30:13 +1200

    added in the task manager html files to get template inheritance

--- app.py
+++ app.py
@@ -1,108 +1,103 @@
 import os
 import json
-from flask import Flask, render_template, redirect, request,url_for
+import pymongo
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME']='data'
-app.config['MONGO_URI']='mongodb+srv://casey:rOOtUser@cluster0-uoefk.mongodb.net/data?retryWrites=true&w=majority'
+app.config['MONGO_DBNAME'] = 'data'
+app.config['MONGO_URI'] = ('mongodb+srv://casey:rOOtUser@cluster0-uoefk.mongodb.net/data?retryWrites=true&w=majority')
 
-mongo=PyMongo(app)
+mongo = PyMongo(app)
 
-# List of the cuisine categories
-cuisines_json = []
-with open("data/cuisine_category.json", "r") as file:
-    cuisines_json = json.load(file)
-
-
-
-# List of the allergen categories
-allergens_json = []
-with open("data/allergen_category.json", "r") as file:
-    allergens_json = json.load(file)
-
-
-def recipe_database():
-    data = {
-        "name": request.form.get('name'),
-        "cuisine": request.form.getlist('cuisine'),
-        "allergens": request.form.getlist('allergens'),
-        "description": request.form.get('description'),
-        "ingredients": request.form.getlist('ingredient'),
-        "instructions": request.form.getlist('instruction'),
-        "prep_time": request.form.get('prep_time'),
-        "cook_time": request.form.get('cook_time'),
-        "recipe_yield": request.form.get('recipe_yield'),
-        "author": request.form.get('author'),
-        "image": request.form.get('image'),
-        "username": session['user']
-    }
-    return data
-
-
-def registration_form():
-    data = {
-        "first_name": request.form.get('register_first_name'),
-        "last_name": request.form.get('register_last_name'),
-        "username": request.form.get('register_username'),
-        "email": request.form.get('register_email'),
-        "password": request.form.get('register_password'),
-        "liked_recipes": []
-    }
-    return data
+#---- Tasks ---------
 
 @app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template('tasks.html', tasks=mongo.db.tasks.find())
+   
+#-------------------------------edited for recipe
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html', cuisines=mongo.db.cuisines.find())
+   
+   
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks=mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
   
-@app.route('/get_recipes')
-def get_recipes():
-  return render_template('recipes.html', recipe= mongo.db.recipe.find())
-
-@app.route("/add_recipe")
-def add_recipe():
-    return render_template(
-        "addrecipe.html",
-        cuisines_json=cuisines_json,
-        allergens_json=allergens_json)
-
-
-@app.route("/recipes")
-def recipes():
-  
-  return render_template(
-        'recipes.html',
-        all_recipes=all_recipes,
-        new_recipes=new_recipes,
-        most_popular_recipes=most_popular_recipes,
-        cuisines_json=cuisines_json,
-        allergens_json=allergens_json,
-        usernames=usernames)
-
-
-@app.route("/insert_recipe", methods=['POST'])
-def insert_recipe():
-  recipe=mongo.db.recipe
-  recipe.insert_one(request.form.to_dict())
-  return redirect(url_for("get_recipe"))
+    
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+    
+   
+@app.route('/update_task/<task_id>', methods=['POST'])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)},
+    {
+        'task_name': request.form.get('task_name'),
+        'category_name': request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent': request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_tasks'))
+    
+    
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
 
 
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+    categories = mongo.db.categories.find())
 
 
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category = mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+    
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
 
 
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id':ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
 
 
-
-
-
-
-
-
-
-
-
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    categories = mongo.db.categories
+    category_doc = {'category_name': request.form.get('category_name')}
+    categories.insert_one(category_doc)
+    return redirect (url_for('get_categories'))
+    
+    
+@app.route('/new_category')
+def new_category():
+    return render_template('addcategory.html')
 
 if __name__ == '__main__':
-  app.run(port=5000,
-          debug=True)
+    app.run(
+            debug=True)
